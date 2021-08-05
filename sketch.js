42aaf2efa4475caf2ac0be0f1dfa3c5f28df1116
guruarora <73347238+guruarora@users.noreply.github.com>
2021-08-05 10:38:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+//data types = string, number, boolean, undefined, null
+
+//string (" ")
+var a = "GuruPrasadh"
+console.log(a);
+
+var a1 = "GURU"
+
+//number
+var b = 123;
+console.log(b);
+
+//boolean - true/false
+var c = true;
+console.log(c);
+
+//undefined
+var d;
+console.log(d);
+
+//null
+var e = null;
+console.log(e);
+
+//converts the lowercase letters to uppercase letters
+console.log(a.toUpperCase())
+
+console.log(a1.toLowerCase())
+
+//display a number without creating a variable --> parseInt()
+console.log(parseInt("66"))
+
+//conditional programming --> if condition
+//to check if a number is greater or not --> if 3 greater than 4
+
+if(3>4){
+  console.log("correct")
+}
+else{     //opposite of if
+  console.log("incorrect")
+}
+
+//ARITHMETIC OPERATORS --> + - / *
+console.log(546+1237)
+console.log(546-1237)
+console.log(546/1237)
+console.log(546*1237)
+
+//comparitive OPERATORS 
+console.log(12>19) 
+console.log(12<19)
+console.log(12===12)
+console.log(7!==8)
+
+//for loop - display numbers from 1 to 10 
+for(var n=1;n<=10;n=n+1){
+  console.log(n)
+}
+
+// to display numerb from 35 to 45
+for(var l=35;l<=45;l=l+1){
+  console.log(l)
+}
+
+
+// to display odd numbers from 1 to 20  using whi le loop
+
+var s=1
+while(s<20){
+  console.log(s)
+s=s+2
+
+}
+
+
+
+
+
+
+
+
